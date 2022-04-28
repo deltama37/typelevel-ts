589f2c2295a2d0832aa57fc5bcf11f487b94a611
ryota misumi <3delta7@gmail.com>
2022-04-29 02:17:23 +0900

    add setting

---
+++ src/plus/index.spec.ts
@@ -0,0 +1,4 @@
+import { plus } from ".";
+test("plus", () => {
+  expect(plus(1, 2)).toBe(3);
+});
--- test/plus.test.ts
+++
@@ -1,5 +0,0 @@
-import { plus, } from "../src/plus";
-
-test("plus", () => {
-    expect(plus(1, 2)).toBe(3);
-});
